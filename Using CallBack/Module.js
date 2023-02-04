b35cd2f3458e1cca71705efe8fe82059fcda8b87
Piyush <piyushpandita151@gmail.com>
2023-02-04 16:42:14 +0530

    CallBack Module

--- Using CallBack/Module.js
+++ Using CallBack/Module.js
@@ -1,6 +1,6 @@
 module.exports = (x, y, callBack) =>{
     if (x==0 || y==0) {
-        callBack(new Error("Both x and y are zeros", 
+        callBack(new Error("Any of the x, y are zeros", 
         null));
     }
     else{
--- Using CallBack/app.js
+++ Using CallBack/app.js
@@ -1,18 +1,28 @@
 var rect = require("./Module");
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-function solve(x, y){
-    console.log("Solving the Add/Sub");
-
-    rect(x, y, (err, solution)=>{
-        if(err){
-            console.log("ERROR: "+ err.message);
-        }
-        else{
-            console.log("Add: "+solution.add());
-            console.log("Sub: "+solution.sub());
-        }
+function solve(){
+    readline.question('Enter first number: ', firstNum => {
+        readline.question('Enter second number: ', secondNum => {
+        console.log("Solving the Add/Sub");
+          let x = parseInt(firstNum);
+          let y = parseInt(secondNum);
+          rect(x, y, (err, solution)=>{
+            if(err){
+                console.log("ERROR: "+ err.message);
+                readline.close();
+            }
+            else{
+                console.log("Add: "+solution.add());
+                console.log("Sub: "+solution.sub());
+                readline.close();
+            }
+        });
+        });
     });
+    
 }
-
-solve(0, 0);
-solve(4, 5);
+solve();
